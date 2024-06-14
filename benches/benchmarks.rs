a70ad2460381e11a6d6c2ada97a116b67a7d6b7b
John F <11171026+lprekon@users.noreply.github.com>
2024-06-14 02:52:06 -0400

    benchmarks updating coefficients and benchmarks updating knots

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -19,3 +19,26 @@ fn bench_backward(b: &mut Bencher) {
     let error = vec![1.0, 2.0];
     b.iter(|| layer.backward(&error));
 }
+
+#[bench]
+fn bench_update(b: &mut Bencher) {
+    let mut layer = KanLayer::new(3, 2, 3, 4);
+    let input = vec![1.0, 2.0, 3.0];
+    let _ = layer.forward(&input);
+    let error = vec![1.0, 2.0];
+    let _ = layer.backward(&error);
+    b.iter(|| layer.update(0.1));
+}
+
+#[bench]
+fn bench_update_knots_from_samples(b: &mut Bencher) {
+    let mut layer = KanLayer::new(3, 2, 3, 4);
+    let input = vec![1.0, 2.0, 3.0];
+    for _ in 0..100 {
+        let _ = layer.forward(&input);
+        let error = vec![1.0, 2.0];
+        let _ = layer.backward(&error);
+    }
+
+    b.iter(|| layer.update_knots_from_samples(0.1));
+}
