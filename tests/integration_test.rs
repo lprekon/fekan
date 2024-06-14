764e97f71dae47a34ee61763fa95cee09b829ed7
John F <11171026+lprekon@users.noreply.github.com>
2024-06-14 00:25:33 -0400

    implements moderate regression test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -106,5 +106,50 @@ fn regressor_xy() {
 
 /// build a model and train it on the function f(x, y) = e^(sin(pi*x) * y^2)
 #[test]
-#[ignore]
-fn exp_sin_pix_y_squared() {}
+fn exp_sin_pix_y_squared() {
+    let training_data = (0..1000)
+        .map(|_| {
+            let x = thread_rng().gen_range(-1.0..1.0);
+            let y = thread_rng().gen_range(-1.0..1.0);
+            let label = ((std::f32::consts::PI * x).sin() * (y * y)).exp();
+            Sample {
+                features: vec![x, y],
+                label,
+            }
+        })
+        .collect::<Vec<Sample>>();
+    let validation_data = (0..100)
+        .map(|_| {
+            let x = thread_rng().gen_range(-1.0..1.0);
+            let y = thread_rng().gen_range(-1.0..1.0);
+            let label = ((std::f32::consts::PI * x).sin() * (y * y)).exp();
+            Sample {
+                features: vec![x, y],
+                label,
+            }
+        })
+        .collect::<Vec<Sample>>();
+    let mut untrained_model = Kan::new(&KanOptions {
+        input_size: 2,
+        layer_sizes: vec![3, 2, 1],
+        degree: 3,
+        coef_size: 5,
+        model_type: ModelType::Regression,
+    });
+
+    let untrained_validation_loss = validate_model(&validation_data, &mut untrained_model);
+    let mut trained_model = train_model(
+        untrained_model,
+        training_data,
+        Some(&validation_data),
+        TrainingOptions::default(),
+    )
+    .unwrap();
+    let validation_loss = validate_model(&validation_data, &mut trained_model);
+    assert!(
+        validation_loss < untrained_validation_loss,
+        "Validation loss did not decrease after training. Before training: {}, After training: {}",
+        untrained_validation_loss,
+        validation_loss
+    );
+}
