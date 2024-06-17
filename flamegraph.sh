812473e1e7c847563d55d5243c8719c98242aff5
John F <11171026+lprekon@users.noreply.github.com>
2024-06-17 18:51:41 -0400

    Update flamegraph

--- flamegraph.sh
+++ flamegraph.sh
@@ -3,7 +3,7 @@ sudo cargo flamegraph -- \
 --data data/clipped_data.pkl \
 -o test.model \
 --hidden-layers 24 \
---classes $(cat data/classes.text) \
+--classes $(cat data/classes.txt) \
 -e 10 \
 --learning-rate 0.05 \
 --validate-each-epoch 
