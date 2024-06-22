6de5ec2d49f1dc04a6c1339579353ebf1ce3813b
John F <11171026+lprekon@users.noreply.github.com>
2024-06-22 18:36:14 -0400

    updates the doc comments for TrainingObserver methods

--- src/training_observer.rs
+++ src/training_observer.rs
@@ -1,8 +1,8 @@
 /// Structs implementing this trait can be used to observe the training process .
 pub trait TrainingObserver {
-    /// called at the end of each epoch of training with the epoch count, the training loss of the epoch, and the validation loss of the epoch
+    /// called by [`crate::train_model`] at the end of each epoch. validation loss will be NaN if no validation set is provided.
     fn on_epoch_end(&self, epoch: usize, epoch_loss: f32, validation_loss: f32);
 
-    /// called at the end of each sample of training. Useful for logging or debugging
+    /// called by [`crate::train_model`] at the end of each sample. useful for logging or benchmarking.
     fn on_sample_end(&self);
 }
