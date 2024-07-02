25bfb46c9c78bda1614694d14beae3c0a283adc5
John F <11171026+lprekon@users.noreply.github.com>
2024-07-01 23:46:19 -0400

    reorganize existing data and benches

--- flamegraph.sh
+++
@@ -1,9 +0,0 @@
-sudo cargo flamegraph --features="serialization" -- \
-build classifier \
---data data/clipped_data.pkl \
---no-save \
---hidden-layer-sizes 24 \
---classes $(cat data/classes.txt) \
--e 2 \
---learning-rate 0.05 \
---validate-each-epoch 
