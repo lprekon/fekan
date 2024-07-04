3e01e83f71880ddd35928c36df327672cf95593f
John F <11171026+lprekon@users.noreply.github.com>
2024-07-03 23:34:24 -0400

    re-enable regression

--- benches/test_ellipj_accuracy.sh
+++ benches/test_ellipj_accuracy.sh
@@ -34,18 +34,18 @@ echo $(git rev-parse HEAD) > $LOG_FILE
 echo "ensuring python packages are installed"
 pip3 install -r requirements.txt
 
-#echo "generating data"
-#python3 generate_ellipj_data.py 1000000 > $DATA_FILE
+echo "generating data"
+python3 generate_ellipj_data.py 1000000 > $DATA_FILE
 
-#echo "running regression"
-#fekan build regressor --data $DATA_FILE \
-#    --hidden-layer-sizes "2,2" \
-#    --learning-rate 0.001 \
-#    --validate-each-epoch \
-#    --log-output \
-#    --no-save \
-#    > $LOG_FILE
-#echo "regression complete"
+echo "running regression"
+fekan build regressor --data $DATA_FILE \
+   --hidden-layer-sizes "2,2" \
+   --learning-rate 0.001 \
+   --validate-each-epoch \
+   --log-output \
+   --no-save \
+   > $LOG_FILE
+echo "regression complete"
 
 if [ -n $S3_BUCKET ]; then
     echo "uploading log file to s3"
