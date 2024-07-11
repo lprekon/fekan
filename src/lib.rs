6fdb6e451d23d722bb7e44e4e72211762192a601
John F <11171026+lprekon@users.noreply.github.com>
2024-07-11 16:21:34 -0400

    simplifies nll loss function

--- src/lib.rs
+++ src/lib.rs
@@ -332,64 +332,26 @@ pub fn validate_model<T: TrainingObserver>(
 /// Returns the negative log liklihood loss and the gradient of the loss with respect to the logits,
 fn calculate_nll_loss_and_gradient(logits: &Vec<f32>, label: usize) -> (f32, Vec<f32>) {
     // calculate the classification probabilities
-    let (logit_max, logit_max_index) = {
+    let logit_max = {
         let mut max = f32::NEG_INFINITY;
-        let mut max_index = 0;
-        for (i, &logit) in logits.iter().enumerate() {
+        for &logit in logits.iter() {
             if logit > max {
                 max = logit;
-                max_index = i;
             }
         }
-        (max, max_index)
+        max
     };
     let norm_logits = logits.iter().map(|&x| x - logit_max).collect::<Vec<f32>>(); // subtract the max logit to prevent overflow
     let counts = norm_logits.iter().map(|&x| x.exp()).collect::<Vec<f32>>();
+
     let count_sum = counts.iter().sum::<f32>();
     let probs = counts.iter().map(|&x| x / count_sum).collect::<Vec<f32>>();
 
-    // calculate the loss
     let logprobs = probs.iter().map(|&x| x.ln()).collect::<Vec<f32>>();
     let loss = -logprobs[label];
 
-    // calculate the error
-    // should I multiply the actual loss in here? intuitively it feels like I should, but the math doesn't tell me I must
-    let dlogprobs = (0..probs.len())
-        .map(|i| if i == label { -1.0 } else { 0.0 })
-        .collect::<Vec<f32>>(); // dloss/dlogpobs. vector is 0 except for the correct class, where it's -1
-
-    let dprobs = probs
-        .iter()
-        .zip(dlogprobs.iter())
-        .map(|(&p, &dlp)| dlp / p)
-        .map(|x| {
-            if x.is_infinite() {
-                if x.is_sign_positive() {
-                    f32::MAX
-                } else {
-                    f32::MIN
-                }
-            } else {
-                x
-            }
-        })
-        .collect::<Vec<f32>>(); // dloss/dprobs = dlogprobs/dprobs * dloss/dlogprobs. d/dx ln(x) = 1/x. dlogprobs/dprobs = 1/probs, `dlp` = dloss/dlogprobs
-    let dcounts_sum: f32 = counts
-        .iter()
-        .zip(dprobs.iter())
-        .map(|(&count, &dprob)| -count / (count_sum * count_sum) * dprob)
-        .sum();
-    let dcounts = dprobs
-        .iter()
-        .map(|&dprob| dcounts_sum + dprob / count_sum)
-        .collect::<Vec<f32>>();
-    let dnorm_logits = dcounts
-        .iter()
-        .zip(counts.iter())
-        .map(|(&dcount, &e_norm_logit)| dcount * e_norm_logit)
-        .collect::<Vec<f32>>(); // dloss/dnorm_logits = dloss/dcounts * dcounts/dnorm_logits, dcounts/dnorm_logits = d/dx exp(x) = exp(x), and exp(norm_logits) = counts, so we just use counts rather than recalculating
-    let dlogit_max: f32 = -dnorm_logits.iter().sum::<f32>();
-    let dlogits = dnorm_logits.iter().enumerate().map(|(i, &dnorm_logit)|{if i == logit_max_index {1.0} else {0.0}} * dlogit_max + dnorm_logit).collect::<Vec<f32>>();
+    let mut dlogits = probs;
+    dlogits[label] -= 1.;
 
     (loss, dlogits)
 }
