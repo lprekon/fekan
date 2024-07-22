64f4cff8c88bb4f5cbe36a20dc83db0bf5856edc
John F <11171026+lprekon@users.noreply.github.com>
2024-07-21 21:01:16 -0400

    fix and lint benchmark script

--- benches/benchmark_accuracy.sh
+++ benches/benchmark_accuracy.sh
@@ -1,48 +1,49 @@
+#!/bin/sh
 set -e # exit on error
 set -x # print commands
 
 # make sure we have all the variables and accesses we need
 
-if [ -n "$S3_BUCKET" ]; then
-    echo "S3 target detected. Checking connection..."
-    aws s3 ls s3://$S3_BUCKET
-    if [$? -ne 0]; then
-        echo "Error: could not connect to s3 bucket $S3_BUCKET"
-        exit 1
-    else 
-        echo "Connection successful"
-    fi
-fi
+# if [ -n "$S3_BUCKET" ]; then
+#     echo "S3 target detected. Checking connection..."
+#     aws s3 ls s3://$S3_BUCKET
+#     if [$? -ne 0]; then
+#         echo "Error: could not connect to s3 bucket $S3_BUCKET"
+#         exit 1
+#     else 
+#         echo "Connection successful"
+#     fi
+# fi
 # set our defaults if we don't have them
-if [ -z $BRANCH ]; then
+if [ -z "$BRANCH" ]; then
     BRANCH="main"
 fi
-if [ -n $KNOT_EXTENSION_TARGETS && -n $KNOT_EXTENSION_TIMES]; then
+if [ -n "$KNOT_EXTENSION_TARGETS" ]; then
     KNOT_EXTENSION_FLAG="--knot-extension-targets $KNOT_EXTENSION_TARGETS --knot-extension-times $KNOT_EXTENSION_TIMES"
 fi
-if [ -n $HIDDEN_LAYER_SIZES ]; then
+if [ -n "$HIDDEN_LAYER_SIZES" ]; then
     HIDDEN_LAYER_SIZES_FLAG="--hidden-layer-sizes $HIDDEN_LAYER_SIZES"
 fi
-if [ -n $NUM_EPOCHS ]; then
+if [ -n "$NUM_EPOCHS" ]; then
     NUM_EPOCHS_FLAG="--num-epochs $NUM_EPOCHS"
 fi
-if [ -n $DEGREE ]; then
+if [ -n "$DEGREE" ]; then
     DEGREE_FLAG="--degree $DEGREE"
 fi
-if [ -n $COEFS ]; then
+if [ -n "$COEFS" ]; then
     COEFS_FLAG="--coefs $COEFS"
 fi
+
 # grab the latest code
 git clone https://github.com/lprekon/fekan.git
 cd fekan
-echo "checking out branch $BRANCH"
-git checkout $BRANCH
+git checkout "$BRANCH"
 git rev-parse HEAD
 cargo install fekan --path . --features "serialization"
 
 cd benches
 
-if [ -z $BENCHMARK ]; then
+if [ -z "$BENCHMARK" ]; then
     # error out - we need to know where out data is coming from
     echo "Error: BENCHMARK environment variable not set"
     exit 1
@@ -56,10 +57,10 @@ fi
 LOG_FILE="${BENCHMARK}_accuracy_$BRANCH.log"
 touch $LOG_FILE
 # tag the log file with the git hash
-echo $(git rev-parse HEAD) > $LOG_FILE
+git rev-parse HEAD > $LOG_FILE
 # generate test data
 DATA_FILE=$(mktemp)".json"
-trap "rm -f $DATA_FILE" EXIT
+trap 'rm -f $DATA_FILE' EXIT
 pip3 install -r requirements.txt
 python3 generate_${BENCHMARK}_data.py 1000000 > $DATA_FILE
 
@@ -69,6 +70,7 @@ fekan build regressor \
     $NUM_EPOCHS_FLAG \
     $DEGREE_FLAG \
     $COEFS_FLAG \
+    $KNOT_EXTENSION_FLAG \
     --learning-rate 0.001 \
     --validate-each-epoch \
     --log-output \
