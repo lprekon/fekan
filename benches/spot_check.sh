8143b9c579bee05c15d8568364a80cb885f9e924
John F <11171026+lprekon@users.noreply.github.com>
2024-07-22 14:22:34 -0400

    update spot check to use extension feature

--- benches/spot_check.sh
+++ benches/spot_check.sh
@@ -10,22 +10,22 @@ set -x # print commands
 
 
 DATA_FILE=$(mktemp)".json"
-trap "rm -f $DATA_FILE" EXIT
+trap 'rm -f $DATA_FILE' EXIT
 
 git rev-parse HEAD
 
-python3 generate_exp_data.py 1000 > $DATA_FILE
+python3 generate_exp_data.py 1000 > "$DATA_FILE"
 
 
-cargo run --features serialization -- build regressor --data $DATA_FILE \
+cargo run --features serialization -- build regressor --data "$DATA_FILE" \
    --hidden-layer-sizes "5" \
-   --learning-rate 0.001 \
-   --max-knot-length 1 \
+   --learning-rate 0.01 \
    --validate-each-epoch \
    --num-threads 4 \
    --no-save \
    --degree 3 \
-   --coefs 15 \
+   --knot-extension-targets 10,20,50 \
+   --knot-extension-times 10,20,30 \
    --epochs 100
 
 
