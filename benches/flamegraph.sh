406aa1a5ae57d266a8ce31e01808e3e1467846f7
John F <11171026+lprekon@users.noreply.github.com>
2024-07-22 14:20:59 -0400

    updates benchmarking data

--- benches/flamegraph.sh
+++ benches/flamegraph.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 mv flamegraph.svg flamegraph_old.svg
 DATA_FILE=$(mktemp)".json"
-trap "rm -f $temp_file" EXIT
+trap 'rm -f $DATA_FILE' EXIT
 
-python3 generate_ellipj_data.py 100000 > $DATA_FILE
+python3 generate_ellipj_data.py 100000 > "$DATA_FILE"
 
 sudo cargo flamegraph --features="serialization" -- \
 build regressor \
---data $DATA_FILE \
+--data "$DATA_FILE" \
 --no-save \
---hidden-layer-sizes "2,2" \
--e 5 \
---max-knot-length 1000 \
+--hidden-layer-sizes "2" \
+-e 10 \
+--coefs 5 \
+--knot-extension-targets 20 \
+--knot-extension-times 4 \
 --learning-rate 0.01 \
---validate-each-epoch 
+--validate-each-epoch  \
+--num-threads 1 
 sudo cargo clean
